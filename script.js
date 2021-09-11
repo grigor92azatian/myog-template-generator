f770e5443251b2eab05f4bfdc7ecc6b42cb309c0
Grigor Azatian <34463837+grigor92azatian@users.noreply.github.com>
2021-09-10 21:09:15 -0700

    started work on functions for gathering user input

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("testing");
+
+
+let templateInfo = document.querySelector(".templateInfo");
+
+
+
+function getUserInput(){
+    let length = document.getElementById("length").value;
+    let width = document.getElementById("width").value;
+    let height = document.getElementById("height").value;
+    console.log(length, width, height);
+}
