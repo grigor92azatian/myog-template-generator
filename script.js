5a3bd4882d5ed5ef3a72772f8de3ffcd6461d62d
Grigor Azatian <34463837+grigor92azatian@users.noreply.github.com>
2021-09-10 21:42:10 -0700

    able to extract user input values from all form elements, both radio and text input

--- script.js
+++ script.js
@@ -5,8 +5,27 @@ let templateInfo = document.querySelector(".templateInfo");
 
 
 function getUserInput(){
+    //text input
     let length = document.getElementById("length").value;
     let width = document.getElementById("width").value;
     let height = document.getElementById("height").value;
-    console.log(length, width, height);
+
+    //radio buttons
+    let selectedSA;
+    let selectedTHW;
+
+    let seamAllowance = [...document.getElementsByName("seamAllowance")] //storing node list into an array
+    for(let i=0;i<seamAllowance.length;i++){
+        if(seamAllowance[i].checked){
+            selectedSA = seamAllowance[i].value;
+        }
+    }
+    let topHemWidth = [...document.getElementsByName("topHemWidth")];
+    for(let j=0;j<topHemWidth.length;j++){
+        if(topHemWidth[j].checked){
+            selectedTHW = topHemWidth[j].value;
+        }
+    }
+
+    console.log({selectedSA,selectedTHW,height, width, length});
 }
